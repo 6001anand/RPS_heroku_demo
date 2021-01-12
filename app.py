ba662f06f05c2cda4b3f21d5795a0bbfc4a8d543
Anand Atre <53115901+6001anand@users.noreply.github.com>
2021-01-12 13:55:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import numpy as np
+#from keras.models import load_model
+from flask import Flask, render_template, Response
+from camera import VideoCamera
+
+app = Flask(__name__)
+#model = load_model(r'C:\Users\Admin\Desktop\Py Ppip install roject\RockPaperScissor\model_resnet50.h5')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+        
+def gen(camera):
+    while True:
+        data= camera.get_frame()
+
+        frame=data[0]
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
